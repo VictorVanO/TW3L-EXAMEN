be170a7b4be22bd386172f807bd7fd7d0cff4c6f
VictorVanO <20253@ecam.be>
2022-12-21 16:07:50 +0100

    database update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var connection = mysql.createConnection({
     host        : 'localhost',
     user        : 'root',
     password    : 'root',
-    database    : 'formations'
+    database    : 'voyages'
 });
 connection.connect(function(error) {if (error) console.log(error);});
 
@@ -27,12 +27,11 @@ app.use(session({
 // Set route
 // Méthode HTTP 'get' dont on déclare le chemin et 2 paramètres
 app.get('/', (req, res) => {
-    connection.query("select * from formation;", function(error, result) {
+    connection.query("select * from voyage;", function(error, result) {
         if(error) console.log(error);
-        res.render('home.ejs', {formations: result});
+        res.render('home.ejs');
     });
 }); 
-
 
 var cookieParser = require('cookie-parser');
 app.use(cookieParser());
